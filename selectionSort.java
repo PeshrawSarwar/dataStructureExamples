68c8c2f717bfd0a6b8b9d32f69650eff5e62727e
Peshraw Sarwar <peshrawsarwar@gmail.com>
2021-01-17 21:57:33 +0300

    Selection Sort

--- selectionSort.java
+++ selectionSort.java
@@ -3,13 +3,19 @@ public class selectionSort{
 		int nums[] = {11,23,67,2,100,101,3,5,99,1001,2,28,42};
 
 		for(int i = 0 ; i < nums.length - 1; i++){
+			//minimum index = 0
 				int min_indx = i;
 				for(int j = i+1 ; j < nums.length ; j++){
+					//if number in one index after i is less than the minimum index then :
 						if(nums[j] < nums[min_indx]){
+							//minimum index is equal to the index of minimum number
 								min_indx = j;
 							}
+						//inserting the minimum one to temp variable
 						int temp = nums[min_indx];
+						//inserting the other number to the min_index variable
 						nums[min_indx] = nums[i];
+						//inserting the temp variable which is the minimum value to minimum index
 						nums[i] = temp;
 					}
 
